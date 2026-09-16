5d69571f3fc2cdd62dd779f1ca3a1ad5da5a3c28
John Victor <johnvictorjrdof@gmail.com>
2026-09-16 20:50:23 -0300

    feat: adiciona modelo e controller de usuário

---
+++ src/main/java/br/com/johnfrazao/todolist/user/UserModel.java
@@ -0,0 +1,28 @@
+package br.com.johnfrazao.todolist.user;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name="tb_users")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    private String username;
+    private String password;
+    private String name;
+
+    @CreationTimestamp
+    private LocalDateTime creatAt;
+
+}
