c85747f5d1872e38cba033c3d3a1c2104f8bd4f1
John Victor <johnvictorjrdof@gmail.com>
2026-09-16 22:48:36 -0300

    feat: adiciona TaskModel

--- src/main/java/br/com/johnfrazao/todolist/user/UserController.java
+++ src/main/java/br/com/johnfrazao/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.johnfrazao.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +25,10 @@ public class UserController {
             // Status code
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+        var passwordHashred = BCrypt.withDefaults()
+                .hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
